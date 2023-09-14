b4f5b5e0ed741d1eb51658c5cf21f902d963602d
Dominik Hulin <109801079+Fattcat@users.noreply.github.com>
2023-09-14 19:32:15 +0000

    Update PyHider.py

--- PyHider.py
+++ PyHider.py
@@ -8,162 +8,116 @@
 # - platform
 # - subprocess
 
-# Modules
-# +------------------------------------+
-import os              	             #-+
-from time import sleep 	             #-+
-import platform        	             #-+
-import subprocess                    #-+
-from colorama import Fore, init      #-+
-from pyfiglet import figlet_format   #-+
-# +------------------------------------+
+import os
+import platform
+from time import sleep
+from colorama import Fore, init
+from pyfiglet import figlet_format
 
-
-# Colors
-# +------------------------------------+
-black = '\033[30m'	                 #-+
-red = '\033[31m'	                 #-+
-green = '\033[32m'	                 #-+
-yellow = '\033[33m'	                 #-+
-orange = '\033[33m\033[31m'          #-+
-blue = '\033[34m'	                 #-+
-magenta = '\033[35m'	             #-+
-cyan = '\033[36m'	                 #-+
-white = '\033[37m'	                 #-+
-reset = '\033[0m'                    #-+
-# +------------------------------------+
-
-# StegHide Commands
-Hide_Steg_Password = ''
-Extr_Steg_Password = ''
-
-# Clear by OS
-# +------------------------------------+
-def Linux_CLEAR():                   #-+
-    sleep(1)                         #-+
-    os.system('clear')               #-+
-    sleep(0.2)                       #-+
-                                     #-+
-def Windows_CLEAR():                 #-+
-    sleep(1)                         #-+
-    os.system('cls')                 #-+
-    sleep(0.2)                       #-+
-# +------------------------------------+
-
-#Defined Text For show in Terminal Linux ONLY ! - Sorry Windows :/
-
-# Detecting OS
-def CheckOS():
-    OperationSystem = platform.system()
-    release = platform.release()
-
-    if OperationSystem == 'Windows':
-        if '10' in release:
-            detected_os = 'Windows 10'
-            Windows_CLEAR()
-        else:
-            detected_os = 'Windows (Version Lower than Windows 10)'
-            Windows_CLEAR()
-    elif OperationSystem == 'Linux':
-        Linux_CLEAR()
-        if 'kali' in release.lower():
-            detected_os = 'Kali Linux'
-            Linux_CLEAR()
-        elif 'arch' in release.lower():
-            detected_os = 'Arch Linux'
-            Linux_CLEAR()
-        else:
-            detected_os = 'Linux'
-            Linux_CLEAR()
-    else:
-        detected_os = 'UNKNOWN OS'
-
-# ---------------------------------------------------------------- # 
-#                          Toto asi NEtreba
-#    for ClearIt in "Linux", "arch", "Kali Linux", "Arch Linux":
-#        os.system("clear")
-#        Linux_CLEAR()
-# ---------------------------------------------------------------- #
-    print('                 + ------------------------------------ +')
-    print(f'                         + - {orange}Detected OS{reset} :' ,f'{green}{OperationSystem}{reset} - +')
-    print('                 + ------------------------------------ +')
-    if OperationSystem =="Linux":
-        print('+ ---------------------------------------------------------------------- +')
-        print('   Great You have Linux OS (it is better in some cases than Windows 10)')
-        print('+ ---------------------------------------------------------------------- +')
-        sleep(1)
-        Kali_Linux()
-        sleep(1)
-    elif OperationSystem ==" Windows":
-        print('+ ------------------------------------- +')
-        print('    Bruhh ... Windows But OK Lets GO.')
-        print('+ ------------------------------------- +')
-        sleep(1)
-        Windows_CLEAR()
-        sleep(1)
-    sleep(1)
-
-# Inicializácia knižnice Colorama
+# Inicializace knihovny Colorama
 init(autoreset=True)
 text_to_colorize = "[ + ] PyHider [ + ]"
-# Vytvorenie veľkého textu pomocou pyfiglet
+# Vytvoření velkého textu pomocí pyfiglet
 big_text = figlet_format(text_to_colorize)
 # Farebný text
 colored_text = f"{Fore.GREEN}{big_text}{Fore.RESET}"
 print(colored_text)
 
-def UseMenu():
-    print(f'              [+] {green}Command --> Use {reset}[+]')
-    print(f'+ --------------------------------------------------------- +')
-    print(f'+ Hide_Steg_Password --> HIDE Password TO Your Image        +')
-    print(f'+ Extr_Steg_Password --> GET Hidden Password FROM Image     +')
-    print(f'+    -h or --help    --> Show Again this for Help           +')
-    print(f'+     -e or exit     --> exit this script :D                +')
-    print(f'+ --------------------------------------------------------- +')
+# Funkce pro čištění konzole v různých operačních systémech
+def clear_console():
+    sleep(1)
+    if platform.system() == 'Windows':
+        os.system('cls')
+    else:
+        os.system('clear')
+    sleep(0.2)
 
-Directory = 'home/kali/Desktop/ToHide'
-OutputDirectory = 'home/kali/Desktop/Hidden'
+# Funkce pro kontrolu operačního systému
+def check_os():
+    detected_os = platform.system()
+    release = platform.release()
 
-def MainMenu():
-    print('[+] [-+-] Welcome to PyHider [-+-] [+]')
-    print(f' '*6, '[+]', f'{green}Created by Fattcat{reset}', '[+]')
-    print(f'Select Option or type {green}-h{reset} or {green}--help{reset} for show available commands')
-    FirstUserInput = input('--> ')
-    if FirstUserInput =='-h' or FirstUserInput =='--help' or FirstUserInput =='help':
-        UseMenu()
-        # Path in Code for Hide 
-    elif FirstUserInput == 'Hide_Steg_Password':
-        print('Write name of TextFile')
-        TxtFileName = input('--> ')
-        print('Write new Pass Phrase (save it for Futute)')
-        password = input('--> ')
-        print('Write Image Name with ')
-        ImageName = input('--> ')
-        CommandForHide = f"steghide embed -ef {TxtFileName} -cf {ImageName} -p {password}"
+    if detected_os == 'Windows':
+        if '10' in release:
+            detected_os = 'Windows 10'
+        else:
+            detected_os = 'Windows (verze nižší než Windows 10)'
+    elif detected_os == 'Linux':
+        if 'kali' in release.lower():
+            detected_os = 'Kali Linux'
+        elif 'arch' in release.lower():
+            detected_os = 'Arch Linux'
+        else:
+            detected_os = 'Linux'
+    else:
+        detected_os = 'NEZNÁMÝ OS'
 
-        
-        os.popen(CommandForHide).read()
-        # Path in Code for Show 
-    elif FirstUserInput =='Extr_Steg_Password':
-        print('Write name of TextFile')
-        TxtFileName = input('--> ')
-        print('Write new Pass Phrase (save it for Futute)')
-        password = input('--> ')
-        print('Write Image Name (with Image Exension)')
-        ImageName = input('--> ')
-        CommandForHide = f'steghide extract -sf {password} -p'
-        os.popen(CommandForHide).read()
+    clear_console()
+    print(' ' * 17, '+ ------------------------------------ +')
+    print(f'                         + - {Fore.YELLOW}Detected OS{Fore.RESET}:', f'{Fore.GREEN}{detected_os}{Fore.RESET} - +')
+    print(' ' * 17, '+ ------------------------------------ +')
 
-def Windows_10():
-    Windows_CLEAR()
-    MainMenu()
+    print('+ ------------------------------------------------------------------------ +')
+    print('   Skvělé! Máte operační systém Linux (v některých případech lepší než Windows 10).')
+    print('+ ------------------------------------------------------------------------ +')
 
-
-def Kali_Linux():
-    Linux_CLEAR()
-    MainMenu()
-
-
-CheckOS()
+check_os()
 sleep(2)
-MainMenu()
+
+# Funkce pro hlavní menu (s přidanou smyčkou)
+def main_menu():
+    while True:
+        print(f' ' * 20, '[+] [-+-] Vítejte v aplikaci PyHider [-+-] [+]')
+        print(f' ' * 25, '[+]', f'{Fore.RED}Vytvořil{Fore.RESET}', f'{Fore.BLUE}NĚKDO{Fore.RESET}', '[+]\n')
+        print(f'Vyberte možnost nebo zadejte {Fore.GREEN}-h{Fore.RESET} nebo {Fore.GREEN}--help{Fore.RESET} pro zobrazení dostupných příkazů')
+        first_user_input = input(f'{Fore.YELLOW}--> ')
+        
+        if first_user_input == '-h' or first_user_input == '--help':
+            use_menu()
+        elif first_user_input == 'Hide_Steg_Password':
+            hide_steg_password()
+        elif first_user_input == 'Extr_Steg_Password':
+            extr_steg_password()
+        elif first_user_input == '-e' or first_user_input == 'exit':
+            exit()
+        else:
+            print('Neplatný vstup, zadejte platný příkaz.')
+
+# Funkce pro menu "Use"
+def use_menu():
+    print(f'[+] {Fore.GREEN}Příkaz --> Use {Fore.RESET}[+]')
+    print(f'+ --------------------------------------------------------- +')
+    print(f'+ Hide_Steg_Password --> Skryj heslo do obrázku                +')
+    print(f'+ Extr_Steg_Password --> Získat skryté heslo z obrázku         +')
+    print(f'+    -h nebo --help  --> Zobrazit znovu toto nápovědní menu   +')
+    print(f'+     -e nebo exit   --> Ukončit skript :D                    +')
+    print(f'+ --------------------------------------------------------- +')
+
+# Funkce pro skrytí hesla do obrázku
+def hide_steg_password():
+    print('Zadejte název textového souboru')
+    txt_file_name = input('--> ')
+    print('Zadejte nové heslo (uložte si ho pro budoucnost)')
+    password = input('--> ')
+    print('Zadejte název obrázku')
+    image_name = input('--> ')
+    command_for_hide = f"steghide embed -ef {txt_file_name} -cf {image_name} -p {password}"
+
+    os.popen(command_for_hide).read()
+    input('Stiskněte Enter pro návrat do hlavního menu.')
+
+# Funkce pro získání skrytého hesla z obrázku
+def extr_steg_password():
+    print('Zadejte název textového souboru')
+    txt_file_name = input('--> ')
+    print('Zadejte heslo')
+    password = input('--> ')
+    print('Zadejte název obrázku (s příponou obrázku)')
+    image_name = input('--> ')
+    command_for_extract = f'steghide extract -sf {image_name} -p {password}'
+    os.popen(command_for_extract).read()
+    input('Stiskněte Enter pro návrat do hlavního menu.')
+
+# Spuštění hlavního menu
+
+main_menu()
